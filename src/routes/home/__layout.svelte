15a04312221127dd7a6acadbf4aa1e303c322113
aagtt <9836608+aagtt@users.noreply.github.com>
2022-12-27 12:29:55 -0300

    Starting the project with landing and home page

---
+++ src/routes/home/__layout.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import Navigation from "./navigation.svelte"
+	import Trending from "./trending.svelte"
+</script>
+
+<div class="container">
+	<Navigation />
+	<main class="feed">
+		<slot />
+	</main>
+	<Trending />
+</div>
+
+<style>
+	.container {
+		height: 100vh;
+		max-width: min-content;
+		margin: 0 auto;
+		display: grid;
+		grid-template-columns: min-content 50ch;
+	}
+
+	.feed {
+		border: 1px solid var(--color-border-primary);
+		border-top: none;
+		border-bottom: none;
+	}
+
+	@media (min-width: 1024px) {
+		.container {
+			max-width: 1240px;
+			margin: 0 auto;
+			grid-template-columns: 1fr 50ch 1fr;
+		}
+	}
+</style>
---
+++ src/routes/home/index.svelte
@@ -0,0 +1,15 @@
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<h1>Feed</h1>
+
+<style>
+	h1 {
+		position: sticky;
+		top: 0;
+		padding: var(--spacing-8) var(--spacing-24);
+		font-size: var(--font-24);
+		backdrop-filter: blur(100px);
+	}
+</style>
